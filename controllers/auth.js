b8513fce64a3f7d61b234c427baebb3a76ce8454
john <john.davis.2015.03@gmail.com>
2022-01-05 20:07:32 -0500

    Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,15 +37,42 @@ const createUser = async (req, res = response) => {
   }
 };
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
   const { email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: "login",
-    email,
-    password,
-  });
+  try {
+    const usuario = await User.findOne({ email });
+
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario no existe con ese email",
+      });
+    }
+
+    // Confirmar los passwords
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password incorrecto",
+      });
+    }
+
+    // Generar nuestro JWT
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "please talk to the administrator",
+    });
+  }
 };
 
 const revalidateToken = (req, res = response) => {
